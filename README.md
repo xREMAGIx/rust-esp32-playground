eff0312b6ff5b49e9280e9bf6b641c6dd3b80340
Remagi <tramdo2806@gmail.com>
2023-08-06 17:58:49 +0700

    :sparkles: implement connect to wifi

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # rust-esp32-s2-playground
 
-My Embedded Rust Playground with ESP32-S2
+My Embedded Rust Playground with the ESP32-S2
 
-For each experiment playground, I will put it into one branch.
+Experiment with Connecting to WiFi
+
+Resource that I used:
+https://esp-rs.github.io/book/introduction.html - For setup workspace
+https://esp-rs.github.io/std-training/02_4_hello_board.html - Example
+https://github.com/esp-rs/std-training/tree/main/intro/hardware-check - Example repo
+https://medium.com/@rajeshpachaikani/connect-esp32-to-wifi-with-rust-7d12532f539b - Medium blog
--- build.rs
+++ build.rs
@@ -1,5 +1,19 @@
-// Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
+
+fn main() -> anyhow::Result<()> {
+    // Check if the `cfg.toml` file exists and has been filled out.
+    if !std::path::Path::new("cfg.toml").exists() {
+        anyhow::bail!("You need to create a `cfg.toml` file with your Wi-Fi credentials! Use `cfg.toml.example` as a template.");
+    }
+
+    // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
     Ok(())
